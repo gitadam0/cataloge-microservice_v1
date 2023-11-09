d2174714ed7987d5b7f9d0b745969080388180c2
unknown <nidamh40@gmail.com>
2023-11-09 15:18:33 +0100

    added etequette and repo

---
+++ src/main/java/com/example/Cataloguemicroservice/Repository/EtiquetteRepository.java
@@ -0,0 +1,7 @@
+package com.example.Cataloguemicroservice.Repository;
+
+import com.example.Cataloguemicroservice.Entities.Etequette;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EtiquetteRepository extends JpaRepository<Etequette,Integer> {
+}
