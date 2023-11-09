162edbd5a7c8f1d03952f032ff29ef0294192710
ADAM <adm.mdd@gmail.com>
2023-11-09 13:05:09 +0100

    first commit of spring init

---
+++ src/main/java/com/example/Cataloguemicroservice/CatalogueMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.Cataloguemicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogueMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogueMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Cataloguemicroservice/CatalogueMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Cataloguemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogueMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
