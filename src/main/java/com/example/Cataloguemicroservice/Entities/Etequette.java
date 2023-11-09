531cf095cba07157579dae0391d30103c3aba0b9
ADAM <adm.mdd@gmail.com>
2023-11-09 15:24:31 +0100

    added etiquettes properties

--- src/main/java/com/example/Cataloguemicroservice/Entities/Etequette.java
+++ src/main/java/com/example/Cataloguemicroservice/Entities/Etequette.java
@@ -1,4 +1,16 @@
 package com.example.Cataloguemicroservice.Entities;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "etiquettes")
 public class Etequette {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idEtiquette;
+    private String nameEtiquette;
 }
