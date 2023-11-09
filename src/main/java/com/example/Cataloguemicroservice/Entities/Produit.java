ab6714b293ed4680a6bcb38470c436ac46dfe742
Youssef <benlarbi1youssef@gmail.com>
2023-11-09 15:08:29 +0100

    Produit Model & ProduitRepository

--- src/main/java/com/example/Cataloguemicroservice/Entities/Produit.java
+++ src/main/java/com/example/Cataloguemicroservice/Entities/Produit.java
@@ -1,4 +1,19 @@
 package com.example.Cataloguemicroservice.Entities;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "produits")
+@NoArgsConstructor
 public class Produit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idProduit;
+    private String nomProduit;
+    private double prixProduit;
 }
---
+++ src/main/java/com/example/Cataloguemicroservice/Repository/ProduitRepository.java
@@ -0,0 +1,7 @@
+package com.example.Cataloguemicroservice.Repository;
+
+import com.example.Cataloguemicroservice.Entities.Produit;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProduitRepository extends JpaRepository<Produit, Long> {
+}
