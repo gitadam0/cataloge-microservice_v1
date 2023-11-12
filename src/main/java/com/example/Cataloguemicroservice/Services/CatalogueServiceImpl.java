7ce41a067c0d8792a24f69bd45840f9de734bfe8
ADAM <adm.mdd@gmail.com>
2023-11-12 20:47:26 +0100

    finished catalogue and it's impl

--- src/main/java/com/example/Cataloguemicroservice/Services/CatalogueServiceImpl.java
+++ src/main/java/com/example/Cataloguemicroservice/Services/CatalogueServiceImpl.java
@@ -1,8 +1,11 @@
 package com.example.Cataloguemicroservice.Services;
 
 import com.example.Cataloguemicroservice.Entities.Catalogue;
+import com.example.Cataloguemicroservice.Entities.Produit;
 import com.example.Cataloguemicroservice.Exceptions.CatalogueNotFoundException;
+import com.example.Cataloguemicroservice.Exceptions.ProductNotFoundException;
 import com.example.Cataloguemicroservice.Repository.CatalogueRepository;
+import com.example.Cataloguemicroservice.Repository.ProduitRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,17 +14,41 @@ import java.util.Optional;
 
 @Service
 public class CatalogueServiceImpl implements CatalogueService {
-    @Autowired
-    private CatalogueRepository catalogueRepository;
 
+    private CatalogueRepository catalogueRepository;
+    @Autowired
+    public CatalogueServiceImpl(CatalogueRepository catalogueRepository) {
+        this.catalogueRepository = catalogueRepository;
+    }
 
     @Override
     public List<Catalogue> getCatalogues() {
         return catalogueRepository.findAll();
     }
-    public Catalogue getCatalogueByID(long id) {
+
+    @Override
+    public void deleteCatalogue(Long id) {
+        catalogueRepository.deleteById(id);
+    }
+
+    public Catalogue getCatalogueByID(long id) throws CatalogueNotFoundException {
         Optional<Catalogue> optionalCatalogue = catalogueRepository.findById(id);
         return optionalCatalogue.orElseThrow(() -> new CatalogueNotFoundException("Catalogue not found for ID: " + id));
+    }
+
+    @Override
+    public Catalogue updateCatalogue(long id,Catalogue newCatalogue) throws CatalogueNotFoundException {
+        Catalogue catalogue = catalogueRepository.findById(id).orElseThrow(()->
+                new CatalogueNotFoundException("Catalogue not found to update for ID: " + id));
+        catalogue.setNomCatalogue(newCatalogue.getNomCatalogue());
+        catalogue.setCategories(newCatalogue.getCategories());
+
+        return catalogue;
+    }
+
+    @Override
+    public Catalogue createCatalogue(Catalogue catalogue) {
+        return catalogueRepository.save(catalogue);
     }
 
 
