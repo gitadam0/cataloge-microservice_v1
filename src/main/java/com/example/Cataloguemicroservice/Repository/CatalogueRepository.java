e5630568540f6e7e3bd90aad0f7b7d2f2bb4922f
ADAM <adm.mdd@gmail.com>
2023-11-27 20:13:08 +0100

    deleted class and all crud for Catalogue

--- src/main/java/com/example/Cataloguemicroservice/Repository/CatalogueRepository.java
+++ src/main/java/com/example/Cataloguemicroservice/Repository/CatalogueRepository.java
@@ -1,6 +1,5 @@
 package com.example.Cataloguemicroservice.Repository;
 
-import com.example.Cataloguemicroservice.Entities.Catalogue;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CatalogueRepository extends JpaRepository<Catalogue, Long> {
