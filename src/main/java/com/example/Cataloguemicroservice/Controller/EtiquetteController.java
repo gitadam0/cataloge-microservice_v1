a56c502b788d6854189c55f9055fb6f1615097e5
Youssef <benlarbi1youssef@gmail.com>
2023-11-27 10:38:42 +0100

    finishing the crud of Etiquette Controller

--- src/main/java/com/example/Cataloguemicroservice/Controller/EtiquetteController.java
+++ src/main/java/com/example/Cataloguemicroservice/Controller/EtiquetteController.java
@@ -1,4 +1,43 @@
 package com.example.Cataloguemicroservice.Controller;
 
+import com.example.Cataloguemicroservice.Entities.Etiquette;
+import com.example.Cataloguemicroservice.Exceptions.EntityNotFoundException;
+import com.example.Cataloguemicroservice.Services.Etiquette.EtiquetteService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/etiquettes")
 public class EtiquetteController {
+    private final EtiquetteService etiquetteService;
+
+    public EtiquetteController(EtiquetteService etiquetteService) {
+        this.etiquetteService = etiquetteService;
+    }
+
+    @GetMapping
+    public List<Etiquette> getAllEtiquettes() {
+        return etiquetteService.getEtiquettes();
+    }
+
+    @GetMapping("/{id}")
+    public Etiquette getEtiquetteById(@PathVariable Long id) throws EntityNotFoundException {
+        return etiquetteService.getEtiquetteByID(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteEtiquette(@PathVariable Long id) {
+        etiquetteService.deleteEtiquette(id);
+    }
+
+    @PostMapping
+    public Etiquette createEtiquette(@RequestBody Etiquette etiquette) {
+        return etiquetteService.createEtiquette(etiquette);
+    }
+
+    @PutMapping("/{id}")
+    public Etiquette updateEtiquette(@PathVariable Long id, @RequestBody Etiquette etiquette) throws EntityNotFoundException {
+        return etiquetteService.updateEtiquette(id, etiquette);
+    }
 }
--- src/main/java/com/example/Cataloguemicroservice/Services/Etiquette/EtiquetteServiceImpl.java
+++ src/main/java/com/example/Cataloguemicroservice/Services/Etiquette/EtiquetteServiceImpl.java
@@ -4,10 +4,12 @@ import com.example.Cataloguemicroservice.Entities.Etiquette;
 import com.example.Cataloguemicroservice.Exceptions.EntityNotFoundException;
 import com.example.Cataloguemicroservice.Repository.EtiquetteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class EtiquetteServiceImpl implements EtiquetteService{
 
     private EtiquetteRepository etiquetteRepository;
