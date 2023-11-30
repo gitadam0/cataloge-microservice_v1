4dcaf20a72757a6b5cf4fb41e062eb01444f729d
Youssef <benlarbi1youssef@gmail.com>
2023-11-30 11:06:13 +0100

    adding jUnit to pom

---
+++ src/test/java/com/example/Cataloguemicroservice/Controller/ProductControllerTest.java
@@ -0,0 +1,13 @@
+package com.example.Cataloguemicroservice.Controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductControllerTest {
+
+    @Test
+    void createProduct() {
+
+    }
+}
