7c7fc9a95ae9ca185ed7bf0c5b0ee4a9c61f90f5
ADAM <adm.mdd@gmail.com>
2023-12-04 10:22:10 +0100

    ProductControllerDiffblueTest

--- src/test/java/com/example/Cataloguemicroservice/testClass.java
+++ src/test/java/com/example/Cataloguemicroservice/testClass.java
@@ -4,6 +4,7 @@ package com.example.Cataloguemicroservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /*@SpringBootTest*/
@@ -13,7 +14,8 @@ public class testClass {
     @Test
     public void testAdd() {
         int result = calculator.add(2, 3);
-        assertEquals(2+3, result);
+//        assertEquals(2+3, result);
+        assertThat(result).isEqualTo(5);
     }
 
     @Test
@@ -22,4 +24,15 @@ public class testClass {
         int result = calculator.add(2, 3);
         assertEquals(2, 1);
     }
+
+
+    public class Calculator {
+        public int add(int a, int b) {
+            return a + b;
+        }
+        public int minus(int a, int b) {
+            return a + b;
+        }
+    }
+
 }
